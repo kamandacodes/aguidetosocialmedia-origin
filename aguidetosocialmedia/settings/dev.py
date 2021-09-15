340f442c514dec70192376c01ec2ba9a92ff5a8e
kamandacodes <kamandacode@gmail.com>
2021-09-15 17:19:26 +0100

    new

---
+++ aguidetosocialmedia/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-(we*d7v6r-8n+pcjxbsv0*b(0cck^eds18tko89s#&i+4f*l_6'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
